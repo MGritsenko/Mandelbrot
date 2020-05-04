663fa8da4607c6a8712b761d0c58aae52f16c615
Mikhail Gritsenko <mikhail.gritsenko@hotmail.com>
2020-05-04 23:10:00 +0300

    Add project files.

---
+++ Mandelbrot/mainwindow.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_mainwindow.h"
+
+#include <QWheelEvent>
+#include <QImage>
+#include <QPaintEvent>
+#include <QResizeEvent>
+#include <QPoint>
+#include <thread>
+#include <vector>
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	MainWindow(QWidget *parent = Q_NULLPTR);
+
+protected:
+	virtual void wheelEvent(QWheelEvent* event) override;
+	virtual void resizeEvent(QResizeEvent* event) override;
+
+private:
+	void drawImage();
+	void calculateMandelbrot(double heightChunkStart, double heightChunkEnd, double imageW, double imageH);
+
+	void createJobs();
+
+private:
+	Ui::MainWindowClass m_ui;
+
+	std::unique_ptr<QImage> m_image;
+
+	const double m_minRe = -2.0;
+	const double m_maxRe = 1.0;
+	const double m_minIm = -1.2;
+	double m_maxIm;
+	double m_reFactor;
+	double m_imFactor;
+	double m_scale = 1.0;
+	QPointF m_offset = { 0, 0 };
+	const int m_numThreads = 7;
+	const int m_iterations = 1000;
+	std::vector<std::thread> m_threads;
+	QPointF m_lastMouseMovePosition = {};
+};
